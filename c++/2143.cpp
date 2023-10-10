5dbc4edce42185c646f719e97769193be8a2677a
eujin-shin <eujinshin0124@gmail.com>
2023-10-10 12:24:36 +0900

    23-10-20

--- c++/2143.cpp
+++ c++/2143.cpp
@@ -1,54 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define M 1000001
 using namespace std;
 
-int a[1000]={0, }, b[1000]={0, }, arr[M]={0, };
-vector <int> v;
+vector <int> a, b, v[2];
 
-int bsearch(int a, int b, int k) {
-  int mid, l=a, r=b;
+long long Bisearch(int k) {
+  int l=0, r=v[1].size()-1, mid;
   while(l<=r) {
     mid=(l+r)/2;
-    if(v[mid]==k) return arr[mid]+1;
-    else if(v[mid]<k) l=mid+1;
-    else r=mid-1;
+    if(v[1][mid]<k) l=mid+1;
+    else if(v[1][mid]>k) r=mid-1;
+    else return v[0][mid];
   }
   return 0;
 }
 
 int main() {
   ios_base::sync_with_stdio(false);
-  int n, m, t, k, x, ans=0;
-  cin>>t;
-  cin>>n;
+  long long cnt=0;
+  int t, n, m, x, tmp;
+  cin>>t>>n;
   for(int i=0; i<n; i++) {
-    cin>>a[i]; if(i>0) a[i]+=a[i-1];
+    cin>>x; tmp=a.size();
+    for(int j=1; j<=i; j++) a.push_back(a[tmp-j]+x);
+    a.push_back(x);
   }
   cin>>m;
   for(int i=0; i<m; i++) {
-    cin>>b[i]; if(i>0) b[i]+=b[i-1];
-    v.push_back(b[i]);
-    for(int j=0; j<i; j++) {
-      v.push_back(b[i]-b[j]);
+    cin>>x; tmp=b.size();
+    for(int j=1; j<=i; j++) b.push_back(b[tmp-j]+x);
+    b.push_back(x);
+  }
+  sort(b.begin(), b.end());
+  for(int i=0; i<b.size(); i++) {
+    if(i==0 || b[i]!=b[i-1]) {
+      v[1].push_back(b[i]); v[0].push_back(1);
+    } else {
+      v[0][v[0].size()-1]++;
     }
   }
-  sort(v.begin(), v.end());
-  k=0;
-  for(int i=1; i<v.size(); i++) {
-    if(v[k]!=v[i]) k=i;
-    else {
-      arr[k]+=1; v.erase(v.begin()+i); i--;
-    }
+  for(int i=0; i<a.size(); i++) {
+    cnt+=Bisearch(t-a[i]);
   }
-  for(int i=0; i<n; i++) {
-    for(int j=0; j<=i; j++) {
-      k=a[i]-a[j];
-      if(i==j) k=a[i];
-      ans+=bsearch(0, v.size()-1, t-k);
-    }
-  }
-  cout<<ans<<'\n';
+  cout<<cnt<<'\n';
   return 0;
 }
