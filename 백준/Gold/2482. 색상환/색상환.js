ecf010ede35f74d12d1d261b544b337a1b1180bb
Eujin Shin <88435970+eujin-shin@users.noreply.github.com>
2025-02-21 10:05:53 +0900

    [Gold III] Title: 색상환, Time: 224 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Gold/2482. 색상환/색상환.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const [n, k] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+function solution(n, k) {
+  const MOD = 1000000003;
+  let prev = [[1], [0, 1, 1]];
+  for (let i = 3; i <= n; i++) {
+    const cur = [];
+    for (let j = 0; j <= Math.ceil(i / 2); j++) {
+      cur.push([
+        ((prev[j]?.[0] || 0) + (prev[j]?.[2] || 0)) % MOD,
+        ((prev[j]?.[1] || 0) + (prev[j]?.[3] || 0)) % MOD,
+        (prev[j - 1]?.[0] || 0) % MOD,
+        (prev[j - 1]?.[1] || 0) % MOD,
+      ]);
+    }
+    prev = cur;
+  }
+  return prev[k] ? (prev[k][0] + prev[k][1] + prev[k][2]) % MOD : 0;
+}
+
+console.log(solution(n, k));
