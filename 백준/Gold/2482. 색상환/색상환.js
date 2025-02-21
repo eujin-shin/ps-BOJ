3c1595b8dd115c5616f87687a3687ce2f88d8923
Eujin Shin <88435970+eujin-shin@users.noreply.github.com>
2025-02-21 10:08:38 +0900

    [Gold III] Title: 색상환, Time: 216 ms, Memory: 14652 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.js
+++ 백준/Gold/2482. 색상환/색상환.js
@@ -11,13 +11,14 @@ function solution(n, k) {
   let prev = [[1], [0, 1, 1]];
   for (let i = 3; i <= n; i++) {
     const cur = [];
-    for (let j = 0; j <= Math.ceil(i / 2); j++) {
-      cur.push([
+    const bound = Math.ceil(i / 2);
+    for (let j = 0; j <= bound; j++) {
+      cur[j] = [
         ((prev[j]?.[0] || 0) + (prev[j]?.[2] || 0)) % MOD,
         ((prev[j]?.[1] || 0) + (prev[j]?.[3] || 0)) % MOD,
         (prev[j - 1]?.[0] || 0) % MOD,
         (prev[j - 1]?.[1] || 0) % MOD,
-      ]);
+      ];
     }
     prev = cur;
   }
