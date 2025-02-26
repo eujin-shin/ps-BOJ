0d95cce190efd6e12ec065bb83836e94662ef932
Eujin Shin <88435970+eujin-shin@users.noreply.github.com>
2025-02-26 10:00:40 +0900

    [Gold IV] Title: 문자열 폭발, Time: 628 ms, Memory: 203452 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const [target, search] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((str) => str.split(""));
+
+function solution(target, search) {
+  const getPi = () => {
+    const arr = [0];
+    let x = 0;
+    for (let i = 1; i < search.length; i++) {
+      while (0 < x && search[i] !== search[x]) {
+        x = arr[x - 1];
+      }
+      arr[i] = search[i] === search[x] ? ++x : x;
+    }
+    return arr;
+  };
+
+  const pi = getPi();
+  const stack = [];
+  let t = 0;
+  for (let i = 0; i < target.length; i++) {
+    while (0 < t && target[i] !== search[t]) {
+      t = pi[t - 1];
+    }
+    stack[stack.length] = [target[i] === search[t] ? ++t : 0, target[i]];
+    if (t === search.length) {
+      stack.splice(-t);
+      t = stack[stack.length - 1]?.[0] || 0;
+    }
+  }
+  return stack.length > 0 ? stack.map(([_, v]) => v).join("") : "FRULA";
+}
+
+console.log(solution(target, search));
